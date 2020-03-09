8ebc3f54a5f2935b05bd27a5fd081376d60166d3
Ricardo Almeida <ricardo@fliit.de>
2020-03-09 22:45:02 +0100

    Add method all into repo

--- lib/campaign_poc/repos/campaign_repo.rb
+++ lib/campaign_poc/repos/campaign_repo.rb
@@ -1,7 +1,17 @@
 module CampaignPoc
   module Repos
     class CampaignRepo < ROM::Repository[:campaigns]
+      commands :create,
+        use: :timestamps,
+        plugins_options: {
+          timestamps: {
+            timestamps: %i(created_at updated_at)
+          }
+        }
 
+      def all
+        campaigns.to_a
+      end
     end
   end
 end
