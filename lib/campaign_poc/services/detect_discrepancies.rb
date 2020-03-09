6302817e8a23c89919198df3530a6f6fd04d6225
Ricardo Almeida <ricardo@fliit.de>
2020-03-10 00:18:31 +0100

    Return discrepancies json with local data

--- lib/campaign_poc/services/detect_discrepancies.rb
+++ lib/campaign_poc/services/detect_discrepancies.rb
@@ -3,6 +3,23 @@ module CampaignPoc
     class DetectDiscrepancies
 
       def self.call
+        campaign_repo = CampaignPoc::Repos::CampaignRepo.new
+        campaigns = campaign_repo.all
+        campaigns.map do |campaign|
+          {
+            "remote_reference": campaign.external_reference,
+            "discrepancies": [
+              "status": {
+                "remote": "disabled",
+                "local": campaign.status,
+              },
+              "description": {
+                "remote": "Rails Engineer",
+                "local": campaign.ad_description,
+              }
+            ]
+          }
+        end
       end
     end
   end
--- spec/services/detect_discrepancies_spec.rb
+++ spec/services/detect_discrepancies_spec.rb
@@ -2,9 +2,32 @@ require "spec_helper"
 
 RSpec.describe CampaignPoc::Services::DetectDiscrepancies do
   let(:subject) { described_class }
+  before do
+    repo = CampaignPoc::Repos::CampaignRepo.new
+    repo.create(
+      ad_description: "Ruby on Rails Developer",
+      status: "active",
+      external_reference: 1
+    )
+  end
+
   describe ".call" do
-    it "responds to call" do
-      subject.call
+    it "list campaigns" do
+      expect(subject.call).to match([
+        {
+          "remote_reference": "1",
+          "discrepancies": [
+            "status": {
+              "remote": "disabled",
+              "local": "active"
+            },
+            "description": {
+              "remote": "Rails Engineer",
+              "local": "Ruby on Rails Developer"
+            }
+          ]
+        }
+      ])
     end
   end
 end
